817992ca2a4758a2f017460a6a72236e1d662925
Samuel <samuelgomes318@gmail.com>
2022-07-21 21:05:42 -0300

    initial commit

---
+++ backend/src/main/java/com/samuelgsouza/samuelsdeliver/SamuelsdeliverApplication.java
@@ -0,0 +1,13 @@
+package com.samuelgsouza.samuelsdeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamuelsdeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamuelsdeliverApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/samuelgsouza/samuelsdeliver/SamuelsdeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samuelgsouza.samuelsdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamuelsdeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

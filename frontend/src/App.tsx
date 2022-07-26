bd2a1f2edc0df01295ac0ef1d99e9664582ad4c9
¨SamuelGSouza¨ <samuelgomes318@gmail.com>
2022-07-26 01:45:26 -0300

    finished frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,13 @@
 import './App.css';
-import Footer from './Footer';
-import Home from './Home';
-import Navbar from './Navbar';
 import AppRoutes from './Routes';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <>
       <AppRoutes />
+      <ToastContainer />
     </>
   );
 }
